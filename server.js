68ebb910be6e604f5a3356684e9a34adf6141b62
ujjvalmishra <46989094+ujjvalmishra@users.noreply.github.com>
2019-02-21 18:43:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const request = require('request');
 const express = require('express');
+
 const app = express();
-const {projectRouter,employeeRouter} = require('./routes/index');
 const bodyParser = require('body-parser');
+const { projectRouter, employeeRouter } = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use('/project', projectRouter);
@@ -14,10 +14,10 @@ db.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
   });
 
-app.listen(3000, function () {
- console.log("App listening on port 3000!");
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
 });
